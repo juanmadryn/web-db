4b09907c39ea8bcc5d75289021253001a00ab354
franpaez <franpaez@daa1a4cf-f140-0410-a25a-23fcb7a039e6>
2007-12-14 17:26:29 +0000

    Deploy modulo infraestructura para fecha 14/12/2007

---
+++ trunk/infraestructura/tags/20071214.0/Java/infraestructura/reglasNegocio/ValidationException.java
@@ -0,0 +1,47 @@
+package infraestructura.reglasNegocio;
+
+import java.util.Vector;
+
+public class ValidationException extends RuntimeException {
+	private Vector<String> stackErrores;
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 3631693394100855853L;
+
+	public ValidationException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ValidationException(Vector<String> vector) {
+		// TODO Auto-generated constructor stub
+		setStackErrores(vector);
+	}
+
+	public ValidationException(String arg0, Vector<String> vector) {
+		super(arg0);
+		setStackErrores(vector);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ValidationException(Throwable arg0, Vector<String> vector) {
+		super(arg0);
+		setStackErrores(vector);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ValidationException(String arg0, Throwable arg1, Vector<String> vector) {
+		super(arg0, arg1);
+		setStackErrores(vector);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Vector<String> getStackErrores() {
+		return stackErrores;
+	}
+
+	public void setStackErrores(Vector<String> mensajeError) {
+		this.stackErrores = mensajeError;
+	}
+
+}
