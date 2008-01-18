81082dcc22c0f1c2c7f49370a07a68b3398e295b
juanmanuelcortez <juanmanuelcortez@daa1a4cf-f140-0410-a25a-23fcb7a039e6>
2008-01-18 17:25:56 +0000

    ADD MENU AND ACCESS.sql - Grant access to Admin into inventario app

---
+++ trunk/inventario/trunk/UML/DBScripts/ADD MENU AND ACCESS.sql
@@ -0,0 +1,16 @@
+﻿-- Data for the 'menu' table
+USE infraestructura;
+START TRANSACTION;
+
+INSERT INTO menu (menu_id,nombre,descripcion,url,grupo) VALUES
+  (70, 'Inventario','Control de Inventario','MenuInventario','Inventario');
+INSERT INTO menu (menu_id,nombre,descripcion,url,grupo) VALUES
+  (7010, 'Clase de Articulo','ABMC Clase de Articulo','AbmcClaseArticulo','Inventario');
+
+INSERT INTO acceso_menu (menu_id,rol_id,user_id,acceso_menu_id) VALUES
+  (70, 1, NULL, 95),
+  (7010, 1, NULL, 96);
+
+ROLLBACK;
+-- Si se ejecuto sin errores, comentar el rollback y descomentar el commit
+-- COMMIT;
