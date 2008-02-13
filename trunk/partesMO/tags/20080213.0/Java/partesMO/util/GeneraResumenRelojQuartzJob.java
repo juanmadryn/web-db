5809d54607a18a90ab3b42f715f6a35cfc9aa519
juanmanuelcortez <juanmanuelcortez@daa1a4cf-f140-0410-a25a-23fcb7a039e6>
2008-02-13 18:33:06 +0000

    partesMO version deployed at Fabri on 13/02/2008

---
+++ trunk/partesMO/tags/20080213.0/Java/partesMO/util/GeneraResumenRelojQuartzJob.java
@@ -0,0 +1,52 @@
+package partesMO.util;
+
+import java.sql.SQLException;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+import partesMO.models.ResumenHorasRelojModel;
+
+import com.salmonllc.sql.DBConnection;
+import com.salmonllc.sql.DataStoreException;
+import com.salmonllc.util.MessageLog;
+
+/**
+ * Quartz job for periodically execute generaResumenRelojes
+ * 
+ * @author fep
+ *
+ */
+public class GeneraResumenRelojQuartzJob implements Job {
+
+	public void execute(JobExecutionContext ctx) throws JobExecutionException {
+		DBConnection conexion = null;
+		try {
+			conexion = DBConnection.getConnection("partesmo","partesmo");
+			ResumenHorasRelojModel dsResHorRlj = new ResumenHorasRelojModel("partesmo","partesmo");
+
+			// The calendar is not really necessary, as we could just
+			// substract (24L * 3600L * 1000L) but i put it anyway only 
+			// to avoid any WTF situation.			
+			GregorianCalendar c = new GregorianCalendar();
+			c.setTime(ctx.getFireTime());
+			c.roll(Calendar.DAY_OF_MONTH, false);			
+			java.sql.Date fecha = new java.sql.Date(c.getTimeInMillis());
+
+			dsResHorRlj.generaResumenRelojes(fecha,fecha,conexion);		
+		} catch (DataStoreException ex) {
+			MessageLog.writeErrorMessage(ex, null);
+		} catch (SQLException ex) {
+			MessageLog.writeErrorMessage(ex, null);
+		} finally {
+			if (conexion != null) {
+				conexion.rollback();
+				conexion.freeConnection();				 
+			}
+		}
+	}
+
+}
