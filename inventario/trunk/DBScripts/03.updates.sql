582e22f62c6c8ee27e0050cc25311e474c355289
juanmanuelcortez <juanmanuelcortez@daa1a4cf-f140-0410-a25a-23fcb7a039e6>
2008-02-25 18:58:48 +0000

    Set access permissions for ordinary users into "Inventario" application.

---
+++ inventario/trunk/DBScripts/03.updates.sql
@@ -0,0 +1,3 @@
+INSERT INTO infraestructura.acceso_menu (acceso_menu_id,rol_id,menu_id,user_id)
+VALUES (104,2,70,null),(105,2,7005,null),(106,2,7020,null),(107,2,7030,null),
+       (108,2,700505,null),(109,2,700510,null),(110,2,700520,null),(111,2,700530,null);
