4835de035d749b9e048568c224a14152f336a26e
franpaez <franpaez@daa1a4cf-f140-0410-a25a-23fcb7a039e6>
2008-03-20 01:53:24 +0000

    Early version of OC generator screen.

---
+++ inventario/trunk/Java/inventario/models/OrdenesCompraModel.java
@@ -0,0 +1,440 @@
+package inventario.models;
+
+import infraestructura.models.BaseModel;
+
+import com.salmonllc.sql.*;
+
+//$CUSTOMIMPORTS$
+//Put custom imports between these comments, otherwise they will be overwritten if the model is regenerated
+
+//$ENDCUSTOMIMPORTS$
+
+/**
+ * OrdenesCompraModel: A SOFIA generated model
+ */
+public class OrdenesCompraModel extends BaseModel {
+
+     /**
+	 * 
+	 */
+	private static final long serialVersionUID = -8803142152331745657L;
+	//constants for columns
+     public static final String ORDENES_COMPRA_ORDEN_COMPRA_ID="ordenes_compra.orden_compra_id";
+     public static final String ORDENES_COMPRA_ENTIDAD_ID_PROVEEDOR="ordenes_compra.entidad_id_proveedor";
+     public static final String ORDENES_COMPRA_USER_ID_COMPRADOR="ordenes_compra.user_id_comprador";
+     public static final String ORDENES_COMPRA_ESTADO="ordenes_compra.estado";
+     public static final String ORDENES_COMPRA_FECHA="ordenes_compra.fecha";
+     public static final String ORDENES_COMPRA_FECHA_ESTIMADA_ENTREGA="ordenes_compra.fecha_estimada_entrega";
+     public static final String ORDENES_COMPRA_FECHA_ENTREGA_COMPLETA="ordenes_compra.fecha_entrega_completa";
+     public static final String ORDENES_COMPRA_DESCRIPCION="ordenes_compra.descripcion";
+     public static final String ORDENES_COMPRA_OBSERVACIONES="ordenes_compra.observaciones";
+
+     //$CUSTOMVARS$
+     //Put custom instance variables between these comments, otherwise they will be overwritten if the model is regenerated
+     
+     //$ENDCUSTOMVARS$
+     
+     /**
+      * Create a new OrdenesCompraModel object.
+      * @param appName The SOFIA application name
+      */
+     public OrdenesCompraModel (String appName) { 
+          this(appName, null);
+     }
+
+     /**
+      * Create a new OrdenesCompraModel object.
+      * @param appName The SOFIA application name
+      * @param profile The database profile to use
+      */
+     public OrdenesCompraModel (String appName, String profile) { 
+          super(appName, profile);
+
+          try {
+
+               //add columns
+               addColumn(computeTableName("ordenes_compra"),"orden_compra_id",DataStore.DATATYPE_INT,true,true,ORDENES_COMPRA_ORDEN_COMPRA_ID);
+               addColumn(computeTableName("ordenes_compra"),"entidad_id_proveedor",DataStore.DATATYPE_INT,false,true,ORDENES_COMPRA_ENTIDAD_ID_PROVEEDOR);
+               addColumn(computeTableName("ordenes_compra"),"user_id_comprador",DataStore.DATATYPE_INT,false,true,ORDENES_COMPRA_USER_ID_COMPRADOR);
+               addColumn(computeTableName("ordenes_compra"),"estado",DataStore.DATATYPE_STRING,false,true,ORDENES_COMPRA_ESTADO);
+               addColumn(computeTableName("ordenes_compra"),"fecha",DataStore.DATATYPE_DATE,false,true,ORDENES_COMPRA_FECHA);
+               addColumn(computeTableName("ordenes_compra"),"fecha_estimada_entrega",DataStore.DATATYPE_DATE,false,true,ORDENES_COMPRA_FECHA_ESTIMADA_ENTREGA);
+               addColumn(computeTableName("ordenes_compra"),"fecha_entrega_completa",DataStore.DATATYPE_DATE,false,true,ORDENES_COMPRA_FECHA_ENTREGA_COMPLETA);
+               addColumn(computeTableName("ordenes_compra"),"descripcion",DataStore.DATATYPE_STRING,false,true,ORDENES_COMPRA_DESCRIPCION);
+               addColumn(computeTableName("ordenes_compra"),"observaciones",DataStore.DATATYPE_STRING,false,true,ORDENES_COMPRA_OBSERVACIONES);
+
+               //set order by
+               setOrderBy(computeTableAndFieldName("ordenes_compra.orden_compra_id") + " ASC");
+
+               //add validations
+               addRequiredRule(ORDENES_COMPRA_FECHA,"La fecha es obligatoria");
+          }
+          catch (DataStoreException e) {
+               com.salmonllc.util.MessageLog.writeErrorMessage(e,this);
+          }
+
+          //$CUSTOMCONSTRUCTOR$
+          //Put custom constructor code between these comments, otherwise it be overwritten if the model is regenerated
+
+          //$ENDCUSTOMCONSTRUCTOR$
+
+     }
+
+     /**
+      * Retrieve the value of the ordenes_compra.orden_compra_id column for the current row.
+      * @return int
+      * @throws DataStoreException
+      */ 
+     public int getOrdenesCompraOrdenCompraId() throws DataStoreException {
+          return  getInt(ORDENES_COMPRA_ORDEN_COMPRA_ID);
+     }
+
+     /**
+      * Retrieve the value of the ordenes_compra.orden_compra_id column for the specified row.
+      * @param row which row in the table
+      * @return int
+      * @throws DataStoreException
+      */ 
+     public int getOrdenesCompraOrdenCompraId(int row) throws DataStoreException {
+          return  getInt(row,ORDENES_COMPRA_ORDEN_COMPRA_ID);
+     }
+
+     /**
+      * Set the value of the ordenes_compra.orden_compra_id column for the current row.
+      * @param newValue the new item value
+      * @throws DataStoreException
+      */ 
+     public void setOrdenesCompraOrdenCompraId(int newValue) throws DataStoreException {
+          setInt(ORDENES_COMPRA_ORDEN_COMPRA_ID, newValue);
+     }
+
+     /**
+      * Set the value of the ordenes_compra.orden_compra_id column for the specified row.
+      * @param row which row in the table
+      * @param newValue the new item value
+      * @throws DataStoreException
+      */ 
+     public void setOrdenesCompraOrdenCompraId(int row,int newValue) throws DataStoreException {
+          setInt(row,ORDENES_COMPRA_ORDEN_COMPRA_ID, newValue);
+     }
+
+     /**
+      * Retrieve the value of the ordenes_compra.entidad_id_proveedor column for the current row.
+      * @return int
+      * @throws DataStoreException
+      */ 
+     public int getOrdenesCompraEntidadIdProveedor() throws DataStoreException {
+          return  getInt(ORDENES_COMPRA_ENTIDAD_ID_PROVEEDOR);
+     }
+
+     /**
+      * Retrieve the value of the ordenes_compra.entidad_id_proveedor column for the specified row.
+      * @param row which row in the table
+      * @return int
+      * @throws DataStoreException
+      */ 
+     public int getOrdenesCompraEntidadIdProveedor(int row) throws DataStoreException {
+          return  getInt(row,ORDENES_COMPRA_ENTIDAD_ID_PROVEEDOR);
+     }
+
+     /**
+      * Set the value of the ordenes_compra.entidad_id_proveedor column for the current row.
+      * @param newValue the new item value
+      * @throws DataStoreException
+      */ 
+     public void setOrdenesCompraEntidadIdProveedor(int newValue) throws DataStoreException {
+          setInt(ORDENES_COMPRA_ENTIDAD_ID_PROVEEDOR, newValue);
+     }
+
+     /**
+      * Set the value of the ordenes_compra.entidad_id_proveedor column for the specified row.
+      * @param row which row in the table
+      * @param newValue the new item value
+      * @throws DataStoreException
+      */ 
+     public void setOrdenesCompraEntidadIdProveedor(int row,int newValue) throws DataStoreException {
+          setInt(row,ORDENES_COMPRA_ENTIDAD_ID_PROVEEDOR, newValue);
+     }
+   
+     /**
+      * Retrieve the value of the ordenes_compra.user_id_comprador column for the current row.
+      * @return int
+      * @throws DataStoreException
+      */ 
+     public int getOrdenesCompraUserIdComprador() throws DataStoreException {
+          return  getInt(ORDENES_COMPRA_USER_ID_COMPRADOR);
+     }
+
+     /**
+      * Retrieve the value of the ordenes_compra.user_id_comprador column for the specified row.
+      * @param row which row in the table
+      * @return int
+      * @throws DataStoreException
+      */ 
+     public int getOrdenesCompraUserIdComprador(int row) throws DataStoreException {
+          return  getInt(row,ORDENES_COMPRA_USER_ID_COMPRADOR);
+     }
+
+     /**
+      * Set the value of the ordenes_compra.user_id_comprador column for the current row.
+      * @param newValue the new item value
+      * @throws DataStoreException
+      */ 
+     public void setOrdenesCompraUserIdComprador(int newValue) throws DataStoreException {
+          setInt(ORDENES_COMPRA_USER_ID_COMPRADOR, newValue);
+     }
+
+     /**
+      * Set the value of the ordenes_compra.user_id_comprador column for the specified row.
+      * @param row which row in the table
+      * @param newValue the new item value
+      * @throws DataStoreException
+      */ 
+     public void setOrdenesCompraUserIdComprador(int row,int newValue) throws DataStoreException {
+          setInt(row,ORDENES_COMPRA_USER_ID_COMPRADOR, newValue);
+     }
+
+     /**
+      * Retrieve the value of the ordenes_compra.estado column for the current row.
+      * @return String
+      * @throws DataStoreException
+      */ 
+     public String getOrdenesCompraEstado() throws DataStoreException {
+          return  getString(ORDENES_COMPRA_ESTADO);
+     }
+
+     /**
+      * Retrieve the value of the ordenes_compra.estado column for the specified row.
+      * @param row which row in the table
+      * @return String
+      * @throws DataStoreException
+      */ 
+     public String getOrdenesCompraEstado(int row) throws DataStoreException {
+          return  getString(row,ORDENES_COMPRA_ESTADO);
+     }
+
+     /**
+      * Set the value of the ordenes_compra.estado column for the current row.
+      * @param newValue the new item value
+      * @throws DataStoreException
+      */ 
+     public void setOrdenesCompraEstado(String newValue) throws DataStoreException {
+          setString(ORDENES_COMPRA_ESTADO, newValue);
+     }
+
+     /**
+      * Set the value of the ordenes_compra.estado column for the specified row.
+      * @param row which row in the table
+      * @param newValue the new item value
+      * @throws DataStoreException
+      */ 
+     public void setOrdenesCompraEstado(int row,String newValue) throws DataStoreException {
+          setString(row,ORDENES_COMPRA_ESTADO, newValue);
+     }
+
+     /**
+      * Retrieve the value of the ordenes_compra.fecha column for the current row.
+      * @return java.sql.Date
+      * @throws DataStoreException
+      */ 
+     public java.sql.Date getOrdenesCompraFecha() throws DataStoreException {
+          return  getDate(ORDENES_COMPRA_FECHA);
+     }
+
+     /**
+      * Retrieve the value of the ordenes_compra.fecha column for the specified row.
+      * @param row which row in the table
+      * @return java.sql.Date
+      * @throws DataStoreException
+      */ 
+     public java.sql.Date getOrdenesCompraFecha(int row) throws DataStoreException {
+          return  getDate(row,ORDENES_COMPRA_FECHA);
+     }
+
+     /**
+      * Set the value of the ordenes_compra.fecha column for the current row.
+      * @param newValue the new item value
+      * @throws DataStoreException
+      */ 
+     public void setOrdenesCompraFecha(java.sql.Date newValue) throws DataStoreException {
+          setDate(ORDENES_COMPRA_FECHA, newValue);
+     }
+
+     /**
+      * Set the value of the ordenes_compra.fecha column for the specified row.
+      * @param row which row in the table
+      * @param newValue the new item value
+      * @throws DataStoreException
+      */ 
+     public void setOrdenesCompraFecha(int row,java.sql.Date newValue) throws DataStoreException {
+          setDate(row,ORDENES_COMPRA_FECHA, newValue);
+     }
+
+     /**
+      * Retrieve the value of the ordenes_compra.fecha_estimada_entrega column for the current row.
+      * @return java.sql.Date
+      * @throws DataStoreException
+      */ 
+     public java.sql.Date getOrdenesCompraFechaEstimadaEntrega() throws DataStoreException {
+          return  getDate(ORDENES_COMPRA_FECHA_ESTIMADA_ENTREGA);
+     }
+
+     /**
+      * Retrieve the value of the ordenes_compra.fecha_estimada_entrega column for the specified row.
+      * @param row which row in the table
+      * @return java.sql.Date
+      * @throws DataStoreException
+      */ 
+     public java.sql.Date getOrdenesCompraFechaEstimadaEntrega(int row) throws DataStoreException {
+          return  getDate(row,ORDENES_COMPRA_FECHA_ESTIMADA_ENTREGA);
+     }
+
+     /**
+      * Set the value of the ordenes_compra.fecha_estimada_entrega column for the current row.
+      * @param newValue the new item value
+      * @throws DataStoreException
+      */ 
+     public void setOrdenesCompraFechaEstimadaEntrega(java.sql.Date newValue) throws DataStoreException {
+          setDate(ORDENES_COMPRA_FECHA_ESTIMADA_ENTREGA, newValue);
+     }
+
+     /**
+      * Set the value of the ordenes_compra.fecha_estimada_entrega column for the specified row.
+      * @param row which row in the table
+      * @param newValue the new item value
+      * @throws DataStoreException
+      */ 
+     public void setOrdenesCompraFechaEstimadaEntrega(int row,java.sql.Date newValue) throws DataStoreException {
+          setDate(row,ORDENES_COMPRA_FECHA_ESTIMADA_ENTREGA, newValue);
+     }
+
+     /**
+      * Retrieve the value of the ordenes_compra.fecha_entrega_completa column for the current row.
+      * @return java.sql.Date
+      * @throws DataStoreException
+      */ 
+     public java.sql.Date getOrdenesCompraFechaEntregaCompleta() throws DataStoreException {
+          return  getDate(ORDENES_COMPRA_FECHA_ENTREGA_COMPLETA);
+     }
+
+     /**
+      * Retrieve the value of the ordenes_compra.fecha_entrega_completa column for the specified row.
+      * @param row which row in the table
+      * @return java.sql.Date
+      * @throws DataStoreException
+      */ 
+     public java.sql.Date getOrdenesCompraFechaEntregaCompleta(int row) throws DataStoreException {
+          return  getDate(row,ORDENES_COMPRA_FECHA_ENTREGA_COMPLETA);
+     }
+
+     /**
+      * Set the value of the ordenes_compra.fecha_entrega_completa column for the current row.
+      * @param newValue the new item value
+      * @throws DataStoreException
+      */ 
+     public void setOrdenesCompraFechaEntregaCompleta(java.sql.Date newValue) throws DataStoreException {
+          setDate(ORDENES_COMPRA_FECHA_ENTREGA_COMPLETA, newValue);
+     }
+
+     /**
+      * Set the value of the ordenes_compra.fecha_entrega_completa column for the specified row.
+      * @param row which row in the table
+      * @param newValue the new item value
+      * @throws DataStoreException
+      */ 
+     public void setOrdenesCompraFechaEntregaCompleta(int row,java.sql.Date newValue) throws DataStoreException {
+          setDate(row,ORDENES_COMPRA_FECHA_ENTREGA_COMPLETA, newValue);
+     }
+
+     /**
+      * Retrieve the value of the ordenes_compra.descripcion column for the current row.
+      * @return String
+      * @throws DataStoreException
+      */ 
+     public String getOrdenesCompraDescripcion() throws DataStoreException {
+          return  getString(ORDENES_COMPRA_DESCRIPCION);
+     }
+
+     /**
+      * Retrieve the value of the ordenes_compra.descripcion column for the specified row.
+      * @param row which row in the table
+      * @return String
+      * @throws DataStoreException
+      */ 
+     public String getOrdenesCompraDescripcion(int row) throws DataStoreException {
+          return  getString(row,ORDENES_COMPRA_DESCRIPCION);
+     }
+
+     /**
+      * Set the value of the ordenes_compra.descripcion column for the current row.
+      * @param newValue the new item value
+      * @throws DataStoreException
+      */ 
+     public void setOrdenesCompraDescripcion(String newValue) throws DataStoreException {
+          setString(ORDENES_COMPRA_DESCRIPCION, newValue);
+     }
+
+     /**
+      * Set the value of the ordenes_compra.descripcion column for the specified row.
+      * @param row which row in the table
+      * @param newValue the new item value
+      * @throws DataStoreException
+      */ 
+     public void setOrdenesCompraDescripcion(int row,String newValue) throws DataStoreException {
+          setString(row,ORDENES_COMPRA_DESCRIPCION, newValue);
+     }
+
+     /**
+      * Retrieve the value of the ordenes_compra.observaciones column for the current row.
+      * @return String
+      * @throws DataStoreException
+      */ 
+     public String getOrdenesCompraObservaciones() throws DataStoreException {
+          return  getString(ORDENES_COMPRA_OBSERVACIONES);
+     }
+
+     /**
+      * Retrieve the value of the ordenes_compra.observaciones column for the specified row.
+      * @param row which row in the table
+      * @return String
+      * @throws DataStoreException
+      */ 
+     public String getOrdenesCompraObservaciones(int row) throws DataStoreException {
+          return  getString(row,ORDENES_COMPRA_OBSERVACIONES);
+     }
+
+     /**
+      * Set the value of the ordenes_compra.observaciones column for the current row.
+      * @param newValue the new item value
+      * @throws DataStoreException
+      */ 
+     public void setOrdenesCompraObservaciones(String newValue) throws DataStoreException {
+          setString(ORDENES_COMPRA_OBSERVACIONES, newValue);
+     }
+
+     /**
+      * Set the value of the ordenes_compra.observaciones column for the specified row.
+      * @param row which row in the table
+      * @param newValue the new item value
+      * @throws DataStoreException
+      */ 
+     public void setOrdenesCompraObservaciones(int row,String newValue) throws DataStoreException {
+          setString(row,ORDENES_COMPRA_OBSERVACIONES, newValue);
+     }
+
+	@Override
+	public String getEstadoActual() throws DataStoreException {
+		return getOrdenesCompraEstado();
+	}
+
+	@Override
+	public int getIdRegistro() throws DataStoreException {
+		return getOrdenesCompraOrdenCompraId();
+	}
+     
+     //$CUSTOMMETHODS$
+     //Put custom methods between these comments, otherwise they will be overwritten if the model is regenerated
+     
+     //$ENDCUSTOMMETHODS$
+     
+}
---
+++ inventario/trunk/Jsp/LkpOrdenesCompra.jsp
@@ -0,0 +1,80 @@
+<%@ taglib uri="/WEB-INF/taglib.tld" prefix="salmon"%>
+<%@ page extends="com.salmonllc.jsp.JspServlet"%>
+<html>
+<salmon:page />
+<salmon:body />
+<salmon:form name="pageForm">
+   <%@include file="message.jsp"%>
+   </td>
+   <!--Page Content Begin-->
+   <salmon:datasource name="dsQBE" type="QBE">
+      <salmon:qbecriteria name="buscar" type="complex" columns="*" />
+   </salmon:datasource>
+   <salmon:datasource name="dsOrdenesCompra" type="MODEL"
+      dbprofile="inventario" model="inventario.models.OrdenesCompraModel"
+      autoretrieve="Never">
+      	<salmon:selection>
+			<salmon:selectioncriteria fieldname="ordenes_compra.estado" operator="EQUALS" value="'0008.0001'" connector="or" />
+			<salmon:selectioncriteria fieldname="ordenes_compra.estado" operator="EQUALS" value="'0008.0005'" />
+		</salmon:selection>
+        <salmon:selectioncriteria fieldname="ordenes_compra.estado" operator="EQUAL" value="'0008.0001'" connector="and"/>         
+   </salmon:datasource>
+   <salmon:box name="box1" width="100%">
+      <salmon:searchformdisplaybox name="searchformdisplaybox1"
+         caption="Ordenes de Compra" qbebuilder="dsQBE"
+         listformdisplaybox="listformdisplaybox1"
+         addbuttonvisible="False">
+         <table width="100%">
+            <tr>
+               <td><salmon:text name="buscarCAP5" text="Buscar"
+                  font="ColumnCaptionFont" /></td>
+               <td><salmon:input type="text" name="buscarTE3"
+                  size="30" maxlength="90" datasource="dsQBE:buscar"></salmon:input></td>
+            </tr>
+         </table>
+      </salmon:searchformdisplaybox>
+   </salmon:box>
+   <salmon:box name="box2" width="100%">
+      <salmon:listformdisplaybox name="listformdisplaybox1"
+         mode="Display_single_page" caption=" " width="100%"
+         addbuttonvisible="False" savebuttonvisible="False"
+         datasource="dsOrdenesCompra"
+         lookupreturnexp="ordenes_compra.orden_compra_id"
+         lookupdescreturnexp="ordenes_compra.orden_compra_id"
+         searchformdisplaybox="searchformdisplaybox1">
+         <salmon:datatable name="datatable1" width="100%"
+            rowsperpage="5" datasource="dsOrdenesCompra">
+            <salmon:datatableheader>
+               <salmon:tr>
+                  <salmon:td>
+                     <salmon:text name="ordenCompraIdCAP10" text="ID"
+                        font="TableHeadingFont" />
+                  </salmon:td>
+                  <salmon:td>
+                     <salmon:text name="ordenCompraDescCAP11" text="Descripción"
+                        font="TableHeadingFont" />
+                  </salmon:td>                  
+               </salmon:tr>
+            </salmon:datatableheader>
+            <salmon:datatablerows>
+               <salmon:tr>
+                  <salmon:td>
+                     <salmon:text name="ordenCompraIdTXT6"
+                        text="orden compra id Goes Here" font="DefaultFont"
+                        datasource="dsOrdenesCompra:ordenes_compra.orden_compra_id" />
+                  </salmon:td>
+                  <salmon:td>
+                     <salmon:text name="ordenCompraDescTXT7"
+                        text="orden compra desc Goes Here" font="DefaultFont"
+                        datasource="dsOrdenesCompra:ordenes_compra.descripcion" />
+                  </salmon:td>                  
+               </salmon:tr>
+            </salmon:datatablerows>
+         </salmon:datatable>
+      </salmon:listformdisplaybox>
+   </salmon:box>
+   <!--Page Content End-->
+</salmon:form>
+<salmon:endbody />
+<salmon:endpage />
+</html>
