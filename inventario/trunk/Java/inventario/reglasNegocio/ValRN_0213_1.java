0efb7e5d8cea5e692e3cd1031181ef51afe11761
juanmanuelcortez <juanmanuelcortez@daa1a4cf-f140-0410-a25a-23fcb7a039e6>
2008-06-13 02:48:53 +0000

    - Se hicieron correción varias. - Se añadió persistencia del monto unitario en los movimientos y del precio de reposición en los resúmenes de saldo.

--- inventario/trunk/Java/inventario/reglasNegocio/ValRN_0213_1.java
+++ inventario/trunk/Java/inventario/reglasNegocio/ValRN_0213_1.java
@@ -23,13 +23,12 @@ import com.salmonllc.sql.DBConnection;
 import com.salmonllc.sql.DataStoreException;
 
 /**
- * @author Francisco
+ * @author Juan Manuel
  * 
- * Regla de negocio asociada al rechazo de una OC
+ * Regla de negocio asociada a la confirmación de una recepción
  * 
  */
-public final class ValRN_0213_1 extends ValidadorReglasNegocio implements
-		Constants {
+public final class ValRN_0213_1 extends ValidadorReglasNegocio {
 
 	/*
 	 * (non-Javadoc)
@@ -41,13 +40,15 @@ public final class ValRN_0213_1 extends ValidadorReglasNegocio implements
 		Statement st = null;
 		try {
 			RecepcionesComprasModel ds = (RecepcionesComprasModel) obj;
-			
-			if(!UsuarioRolesModel.isRolUsuario(ds.getCurrentWebsiteUserId(), USER_ENCARGADO_ALMACEN))
-				throw new DataStoreException("Usted no está autorizado para confirmar recepciones.");
-			
+
+			if (!UsuarioRolesModel.isRolUsuario(ds.getCurrentWebsiteUserId(),
+					Constants.USER_ENCARGADO_ALMACEN))
+				throw new DataStoreException(
+						"Usted no está autorizado para confirmar recepciones.");
+
 			DetalleRCModel detalles = new DetalleRCModel("inventario");
-			DetalleSCModel detallesSC = new DetalleSCModel("inventario");		
-			
+			DetalleSCModel detallesSC = new DetalleSCModel("inventario");
+
 			int recepcionCompraId = ds.getRecepcionesComprasRecepcionCompraId();
 
 			detalles.setOrderBy("detalles_rc.almacen_id");
@@ -61,13 +62,16 @@ public final class ValRN_0213_1 extends ValidadorReglasNegocio implements
 
 			int tipoMovimiento = Props.getProps("inventario", null)
 					.getIntProperty("TipoMovimientoRecepciones");
-			int almacen_id = 0;	
-			
+			int almacen_id = 0;
+
 			detallesSC.retrieve();
 			Hashtable<Integer, Integer> almacen_comprobantes = new Hashtable<Integer, Integer>();
-			
+
 			float cantidad_recibida = 0;
-			do {
+			
+			
+			for (int row = 0; row < detalles.getRowCount(); row++){
+				detalles.gotoRow(row);
 				if (almacen_id != detalles.getDetallesRcAlmacenId()) {
 					almacen_id = detalles.getDetallesRcAlmacenId();
 					comprobanteMovimiento.gotoRow(comprobanteMovimiento
@@ -101,13 +105,13 @@ public final class ValRN_0213_1 extends ValidadorReglasNegocio implements
 				detallesSC.filter("detalle_sc.detalle_SC_id =="
 						+ detalles.getDetallesRcDetalleScId());
 				detallesSC.gotoFirst();
-				
-				cantidad_recibida = detallesSC
-						.getDetalleScCantidadRecibida()+ (float) detalles.getDetallesRcCantidad();
-				
-				detallesSC.setDetalleScCantidadRecibida(cantidad_recibida);				
-			} while (detalles.gotoNext());
-			
+
+				cantidad_recibida = detallesSC.getDetalleScCantidadRecibida()
+						+ (float) detalles.getDetallesRcCantidad();
+
+				detallesSC.setDetalleScCantidadRecibida(cantidad_recibida);
+			}
+			detallesSC.filter(null);
 			detallesSC.update(conn);
 			detallesSC.resetStatus();
 			comprobanteMovimiento.update(conn);
@@ -115,7 +119,15 @@ public final class ValRN_0213_1 extends ValidadorReglasNegocio implements
 
 			st = conn.createStatement();
 			ResultSet rs = st
-					.executeQuery("SELECT sum(detalles_rc.cantidad_recibida) cantidad_recibida, sum(detalles_rc.cantidad_excedencia) cantidad_excedencia, detalle_sc.articulo_id, detalles_rc.almacen_id, solicitudes.proyecto_id, detalle_sc.tarea_id, cantidad_pedida, detalles_rc.unidad_medida_id FROM  detalles_rc detalles_rc inner join recepciones_compras ON detalles_rc.recepcion_compra_id = recepciones_compras.recepcion_compra_id  inner join detalle_sc detalle_sc ON detalles_rc.detalle_sc_id = detalle_sc.detalle_SC_id  inner join almacenes ON detalles_rc.almacen_id = almacenes.almacen_id  inner join articulos articulos ON detalle_sc.articulo_id = articulos.articulo_id  inner join unidades_medida unidades_medida ON detalle_sc.unidad_medida_id = unidades_medida.unidad_medida_id JOIN solicitudes_compra solicitudes ON detalle_sc.solicitud_compra_id = solicitudes.solicitud_compra_id WHERE detalles_rc.recepcion_compra_id = "
+					.executeQuery("SELECT sum(detalles_rc.cantidad_recibida) cantidad_recibida, sum(detalles_rc.cantidad_excedencia) cantidad_excedencia, detalle_sc.articulo_id, detalles_rc.almacen_id, solicitudes.proyecto_id, detalle_sc.tarea_id, cantidad_pedida, detalles_rc.unidad_medida_id, monto_unitario "
+							+ "FROM detalles_rc detalles_rc "
+							+ "INNER JOIN recepciones_compras ON detalles_rc.recepcion_compra_id = recepciones_compras.recepcion_compra_id  "
+							+ "INNER JOIN detalle_sc detalle_sc ON detalles_rc.detalle_sc_id = detalle_sc.detalle_SC_id  "
+							+ "INNER JOIN almacenes ON detalles_rc.almacen_id = almacenes.almacen_id  "
+							+ "INNER JOIN articulos articulos ON detalle_sc.articulo_id = articulos.articulo_id "
+							+ "INNER JOIN unidades_medida unidades_medida ON detalle_sc.unidad_medida_id = unidades_medida.unidad_medida_id "
+							+ "INNER JOIN solicitudes_compra solicitudes ON detalle_sc.solicitud_compra_id = solicitudes.solicitud_compra_id "
+							+ "WHERE detalles_rc.recepcion_compra_id = "
 							+ recepcionCompraId
 							+ " GROUP BY almacen_id, articulo_id ORDER BY almacen_id");
 
@@ -124,11 +136,12 @@ public final class ValRN_0213_1 extends ValidadorReglasNegocio implements
 			int articulo_id = 0;
 			int comprobante_movimiento_id = 0;
 			double cantidad_entregada = 0;
+			int unidad_medida_id = 0;
 
 			while (rs.next()) {
 				almacen_id = rs.getInt("almacen_id");
 				articulo_id = rs.getInt("articulo_id");
-
+				unidad_medida_id = rs.getInt("unidad_medida_id");
 				comprobanteMovimiento
 						.setFindExpression("comprobante_movimiento_articulo.almacen_id == "
 								+ almacen_id);
@@ -138,26 +151,32 @@ public final class ValRN_0213_1 extends ValidadorReglasNegocio implements
 
 				movimiento.gotoRow(movimiento.insertRow());
 				movimiento.setMovimientoArticuloArticuloId(articulo_id);
-				cantidad_entregada = rs.getDouble("cantidad_recibida")	+ rs.getDouble("cantidad_excedencia");
-				movimiento.setMovimientoArticuloCantidadEntregada(cantidad_entregada);
-				movimiento.setMovimientoArticuloCantidadSolicitada(cantidad_entregada);
-				movimiento.setMovimientoArticuloComprobanteMovimientoId(comprobante_movimiento_id);
+				cantidad_entregada = rs.getDouble("cantidad_recibida")
+						+ rs.getDouble("cantidad_excedencia");
+				movimiento
+						.setMovimientoArticuloCantidadEntregada(cantidad_entregada);
+				movimiento
+						.setMovimientoArticuloCantidadSolicitada(cantidad_entregada);
+				movimiento
+						.setMovimientoArticuloComprobanteMovimientoId(comprobante_movimiento_id);
 				movimiento.setMovimientoArticuloProyectoId(rs
 						.getInt("proyecto_id"));
-				movimiento.setMovimientoArticuloUnidadMedidaId(rs
-						.getInt("unidad_medida_id"));
+				movimiento
+						.setMovimientoArticuloUnidadMedidaId(unidad_medida_id);
 
 				movimiento.setMovimientoArticuloTareaId(rs.getInt("tarea_id"));
+				movimiento.setMovimientoArticuloMontoUnitario(rs.getInt("monto_unitario"));
 			}
 			movimiento.update(conn);
 			movimiento.resetStatus();
 
-			int accion = Props.getProps("inventario", null)
-					.getIntProperty(ACCION_CONFIRMA_MOVIMIENTO);
-			
+			int accion = Props.getProps("inventario", null).getIntProperty(
+					Constants.ACCION_CONFIRMA_MOVIMIENTO);
+
 			for (int row = 0; row < comprobanteMovimiento.getRowCount(); row++) {
 				comprobanteMovimiento.gotoRow(row);
-				comprobanteMovimiento.setCurrentWebsiteUserId(ds.getCurrentWebsiteUserId());
+				comprobanteMovimiento.setCurrentWebsiteUserId(ds
+						.getCurrentWebsiteUserId());
 				if (accion > 0)
 					comprobanteMovimiento.ejecutaAccion(accion, "0010", "", ds
 							.getCurrentWebsiteUserId(),
